5654e03256153c040a6c0606f3dd5c77d422f7a9
dhananjay-diriyah <dhananjaya.kumar@publicissapient.com>
2025-06-27 12:42:40 +0530

    UE url update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
       <script src="https://universal-editor-service.adobe.io/cors.js" async></script>
       <meta name="urn:adobe:aue:system:aemconnection" content={`aem65:${process.env.AEM_ON_PREM_HOST_AUTHOR}`}></meta>
-      <meta name="urn:adobe:aue:config:service" content="https://localhost:8008" />
+      <meta name="urn:adobe:aue:config:service" content="http://diriyah-dev1.adobecqms.net/universal-editor" />
         </Head>
       <body className="antialiased">
         <Main />
