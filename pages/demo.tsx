2e71b34572017c7881f0bd2789caa08699876d02
dhananjaya0508 <dhananjay050815@gmail.com>
2025-08-01 13:04:49 +0530

    Update demo.tsx

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -1,7 +1,7 @@
 import ImageWithCTA from "@/components/image-with-cta";
 import Header from '@/components/header';
 import Footer from '@/components/footer';
-
+ 
 import {
   FaFacebookF,
   FaInstagram,
@@ -9,7 +9,7 @@ import {
   FaGithub,
   FaYoutube,
 } from "react-icons/fa6";
-
+ 
 const footerSections = [
   {
     title: "Hospitality",
@@ -44,7 +44,7 @@ const footerSections = [
     ],
   },
 ];
-
+ 
 const socialIcons = [
   { icon: FaFacebookF, href: "#" },
   { icon: FaInstagram, href: "#" },
@@ -52,45 +52,47 @@ const socialIcons = [
   { icon: FaGithub, href: "#" },
   { icon: FaYoutube, href: "#" },
 ];
-
-
+ 
+ 
 interface HomeProps {
   readonly content: {
     pageTitle?: string;
     _path?: string
     pageDescription?: { plaintext: string };
-    componentReference?: any[];
+    componentFragmentReference?: any[];
   } | null;
 }
-
+ 
 export default function Home({ content }: HomeProps) {
-
-const { pageTitle, pageDescription, componentReference } = content || {};
+ 
+const { pageTitle, pageDescription, componentFragmentReference } = content || {};
+ 
+console.log("content : ", JSON.stringify(content));
   return (
     <div>
        <Header />
     <div
       className={`items-center justify-items-center mx-auto`}
     >
-      <main className=" w-full flex flex-col gap-[32px] row-start-2 items-center sm:items-start" 
+      <main className=" w-full flex flex-col gap-[32px] row-start-2 items-center sm:items-start"
       data-aue-type="page"
       data-aue-resource={`urn:aemconnection:${content?._path}/jcr:content/data/master`}
-
+ 
       >
     
-      {componentReference && componentReference.map((component, index) => (
+      {componentFragmentReference && componentFragmentReference.map((component, index) => (
         <ImageWithCTA
           key={component.id || index}
-          title={component.componentTitle}
-          description1={component.componentDescription?.plaintext ?? ''}
-          imageUrl= {`${process.env.AEM_ON_PREM_HOST}${component.componentImage._path}`}
+          title={component.title}
+          description1={component.description1?.plaintext ?? ''}
+          imageUrl= {`${process.env.AEM_ON_PREM_HOST}${component.componentImage.image._path}`}
           imageAlt={component.componentImage?.imageAltText ?? ''}
           variation={index === 0 ? "background" : index % 2 === 0 ? 'left' : 'right'}
           cfPath={component._path}
         />
       ))}
-
-
+ 
+ 
 <div className="container mx-auto flex flex-col items-center gap-3.5 justify-center pb-12" data-aue-type="page"
       data-aue-resource={`urn:aemconnection:${content?._path}/jcr:content/data/master`}>
 <h1
@@ -98,13 +100,13 @@ const { pageTitle, pageDescription, componentReference } = content || {};
         data-aue-type="text"  
         data-cf-field="title"
         className="text-4xl font-bold text-gray-900"
-
+ 
         >{pageTitle}</h1>
-      <p className="text-2xl" data-aue-prop="pageDescription" data-aue-type="text" data-aue-label="pageDescription"> 
+      <p className="text-2xl" data-aue-prop="pageDescription" data-aue-type="text" data-aue-label="pageDescription">
         {pageDescription?.plaintext}
       </p>
         
-
+ 
 </div>
    
        
@@ -122,12 +124,12 @@ const { pageTitle, pageDescription, componentReference } = content || {};
     </div>
   );
 }
-
+ 
 export async function getServerSideProps() {
     console.log("host : " , process.env.AEM_ON_PREM_HOST);
     console.log("endpoint : " , process.env.AEM_ON_PREM_GRAPHQL_ENDPOINT);
     console.log("auth : " , process.env.AEM_ON_PREM_AUTH);
-
+ 
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
   const res = await fetch(`${process.env.AEM_ON_PREM_HOST}${process.env.AEM_ON_PREM_GRAPHQL_ENDPOINT}`, {
     method: 'GET',
@@ -136,7 +138,7 @@ export async function getServerSideProps() {
       ...(process.env.AEM_ON_PREM_AUTH && { Authorization: process.env.AEM_ON_PREM_AUTH }), // Include Authorization only if defined
     }
   });
-
+ 
   const json = await res.json();
   console.log("response : " , json.data);
   if (!res.ok) {
@@ -146,9 +148,9 @@ export async function getServerSideProps() {
       notFound: true,
     };
   }
-
-  const content = json?.data?.diriyahPageContentFragmentByPath?.item || null;
-
+ 
+  const content = json?.data.diriyahPageContentFragmentModelByPath?.item || null;
+ 
   return {
     props: { content },
   };
