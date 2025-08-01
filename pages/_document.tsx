d81efac17c40aa7eef57d232500b9b2f60a8750b
dhananjaya0508 <dhananjay050815@gmail.com>
2025-08-01 12:17:52 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
       <script src="https://universal-editor-service.adobe.io/cors.js" async></script>
       <meta name="urn:adobe:aue:system:aemconnection" content={`aem:${process.env.AEM_ON_PREM_HOST_AUTHOR}`}></meta>
-      
+      <meta name="urn:adobe:aue:config:service" content="https://author-diriyah-dev1.adobecqms.net/universal-editor" />
         </Head>
       <body className="antialiased">
         <Main />
